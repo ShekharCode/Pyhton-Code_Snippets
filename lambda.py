a0a48bdb5ca26ee409cba66013de89c8a2f63d13
Raja Shekhar Potoori <rajashekhariit@gmail.com>
2024-01-21 15:40:31 +0530

    code2

---
+++ lambda.py
@@ -0,0 +1,6 @@
+a = lambda x,y : x+y
+
+print(a(1,2))
+
+b = lambda x : lambda y: x*y
+print(b(2)(4))
---
+++ zip.py
@@ -0,0 +1,7 @@
+names = ['Raja','Uma','Rahini','Sam']
+marks = [34,45,56,51]
+
+z = zip(range(4),names,marks)
+
+for i in z:
+    print(i)
